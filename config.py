7d308da40aa8654569ac8e62ea769fe70e13eb68
MYang <miyang@cgsinc.ca>
2022-03-19 15:15:05 -0300

    tt 000

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ class Config:
     PORT = os.getenv('PORT')
     DATA_SERVICE_HOST = os.getenv('DATA_SERVICE_HOST')
     DATA_SERVICE_PORT = os.getenv('DATA_SERVICE_PORT')
-    TOTAL_AND_ANNUALIZED_OPP_AMOUNT_URL = f"{DATA_SERVICE_HOST}:{DATA_SERVICE_PORT}/api/v1/review/total-opp-amount"
-    PERFORMANCE_CHART_DATA_URL = f"{DATA_SERVICE_HOST}:{DATA_SERVICE_PORT}/api/v1/review/performance-chart-data"
-    OPPORTUNITIES_BOOKED_URL = f"{DATA_SERVICE_HOST}:{DATA_SERVICE_PORT}/api/v1/review/booked-opps"
+    TOTAL_AND_ANNUALIZED_OPP_AMOUNT_URL = f"{DATA_SERVICE_HOST}/api/v1/review/total-opp-amount"
+    PERFORMANCE_CHART_DATA_URL = f"{DATA_SERVICE_HOST}/api/v1/review/performance-chart-data"
+    OPPORTUNITIES_BOOKED_URL = f"{DATA_SERVICE_HOST}/api/v1/review/booked-opps"
 
 appConfig = Config()
