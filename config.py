4d04e8e06eff919d3d29112360ad8cec955cdafa
MYang <miyang@cgsinc.ca>
2022-03-18 22:33:15 -0300

    init commit

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 # config.py
 import os
 
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
 
 class Config:
     PORT = os.getenv('PORT')
